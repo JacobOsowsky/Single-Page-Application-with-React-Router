5ede28c1bd421f999eb6348b7ebd43b0ce63934a
Jacob <j.osowski15@interia.pl>
2021-03-28 18:11:47 +0200

    update footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import '../styles/Footer.css'
 
 const Footer = () => {
     return ( 
         <div>
-            Footer
+            <h4>Footer</h4>
+            <Route path='/' exact render={()=>(
+                <p>Jesteś na <span>stronie głównej</span></p>
+            )}/>
+
+            <Route path='/:page' render={(props)=>(
+                <>
+                <p>Jesteś na <span>{props.match.params.page}</span></p>
+                
+                </>
+            )}/>
+
+            <Route path='/:page/:product' render={(props)=>(
+                <>
+                <p>Jesteś na <span>{props.match.params.product}</span></p>
+                
+                </>
+            )}/>
+
         </div>
      );
 }
